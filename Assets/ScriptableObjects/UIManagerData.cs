c50bc6ac50e5cf04040648ed6a2cc107e526bab6
DESKTOP_Andres <aecheverri.1995@gmail.com>
2023-08-13 12:10:16 -0500

    cambio de orden de rooms, pequeños cambios en archivos

---
+++ Assets/ScriptableObjects/UIManagerData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/UIManagementData", order = 3)]
+public class UIManagerData : ScriptableObject
+{
+    
+    
+}
